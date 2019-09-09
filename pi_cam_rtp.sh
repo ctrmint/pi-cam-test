3cf9fe6b03ce0d0ba6aef1a9f4f4443b87bc99ef
ctrmint <mark@mrodman.com>
2019-09-09 14:39:51 +0100

    Update pi_cam_rtp.sh

--- pi_cam_rtp.sh
+++ pi_cam_rtp.sh
@@ -1,12 +1,15 @@
-!#/bin/bash
+#!/bin/bash
 # Set up some vars
 # Test Pi Zero camera streaming options.
 
 TIMER=0             # zero value, run indefinately.
-WIDTH=800           # display width
-HEIGHT=400          # display height
-FPS=12              # frames per second
-PORT=8160           # tcp port
+WIDTH=640           # display width
+HEIGHT=480          # display height
+FPS=25              # frames per second, more frames per second more cpu time.
+PORT=8554           # tcp port
 ENCODING="h264"     # encoding
+BITRATE=1000000     # bitrate of stream, greater the rate, greater the cpu hit.
+FILENAME="birdbox"  # filename is required on url, eg rtsp://192.168.0.8:8554/birdbox
 
-raspivid -o - -t $TIMER -n -w $WIDTH -h $HEIGHT -fps $FPS | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:$PORT/}' :demux=$ENCODING
+raspivid -o - -t $TIMER -w $WIDTH -h $HEIGHT -fps $FPS -ih -pf baseline -k -b $BITRATE -n | \
+        cvlc -A-vvv alsa,none -v stream:///dev/stdin --sout '#rtp{sdp=rtsp://:'$PORT'/'$FILENAME'}' :demux=h264
